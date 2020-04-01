f71dcea5012b7444577ebd63f6d73db09b0b6ebd
John <morrowjo@oregonstate.edu>
2020-03-31 19:19:02 -0700

    can thrust and rotate

--- Assets/Rocket.cs
+++ Assets/Rocket.cs
@@ -4,10 +4,13 @@ using UnityEngine;
 
 public class Rocket : MonoBehaviour
 {
+    private Rigidbody rigidBody;
+    private const float thrust = 1.2f;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidBody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
@@ -21,15 +24,18 @@ public class Rocket : MonoBehaviour
         if (Input.GetKey(KeyCode.S))
         {
             print("Thrusting");
+            rigidBody.AddRelativeForce(Vector3.up * thrust);
         }
         
         if (Input.GetKey(KeyCode.A))
         {
             print("Rotating Left");
+            transform.Rotate(Vector3.forward);
         }
         else if (Input.GetKey(KeyCode.D))
         {
             print("Rotating Right");
+            transform.Rotate(-Vector3.forward);
         }
     }
 }
